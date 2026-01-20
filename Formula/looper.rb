60a8360f114502683f972cb32336bcb1d5d2dff3
nibzard <wave@nibzard.com>
2026-01-20 20:01:00 +0100

    chore(formula): bump to v0.3.2

--- Formula/looper.rb
+++ Formula/looper.rb
@@ -1,8 +1,8 @@
 class Looper < Formula
   desc "Codex RALF loop runner and skills pack"
   homepage "https://github.com/nibzard/looper"
-  url "https://github.com/nibzard/looper/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "2a231214bf5e39f2f62975ceace0d7e045ac05bf4cec54bcf861992984cb06e3"
+  url "https://github.com/nibzard/looper/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "917a4abcbdfa8c55142600a81e7140875856d26472ce9944beb96bb3ca68e3e8"
 
   head "https://github.com/nibzard/looper.git", branch: "main"
 
